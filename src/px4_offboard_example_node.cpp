c35d91fabdfb04aa33b576dc75bb15a3a2b69cba
rcighsv <rcighsv@gmail.com>
2019-07-28 17:33:53 -0500

    added tag detection

--- src/px4_offboard_example_node.cpp
+++ src/px4_offboard_example_node.cpp
@@ -11,11 +11,17 @@
 #include <mavros_msgs/State.h>
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/CameraInfo.h>
+#include <opencv2/opencv.hpp>
+#include <sensor_msgs/Image.h>
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+#include <sensor_msgs/image_encodings.h>
+using namespace cv;
 
 //#include <apriltags2_ros/AprilTagDetectionArray.h>
 #include <apriltag_ros/AprilTagDetectionArray.h>
 
-
+static const std::string BOTTOM_WINDOW_NAME = "bottom camera";
 using namespace std;
 
 mavros_msgs::State current_state;
@@ -51,43 +57,76 @@ void coord_cb(const geometry_msgs::PoseStamped::ConstPtr& msg)
 
 void tag_detect_cb(const apriltag_ros::AprilTagDetectionArray::ConstPtr& msg)
 {
-    cout << "inside tag_detect_cb" << endl;
+    int size = msg->detections.size();
+    if(size > 0)
+    {
+        for(int i=0;i<size; i++)
+        {
+            cout << "Found AprilTag ID:  " << msg->detections[i].id[0] << endl;
+        }
+    }
+
+    //if(msg->detections != 0)
+ //       cout << "tagID: " << msg->detections[0].id[0] << endl;
+}
+
+void bottomImageCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+	cv_bridge::CvImagePtr cvImagePtr;
+
+	try
+    {
+		cvImagePtr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+	}
+    catch(cv_bridge::Exception& e)
+    {
+		ROS_ERROR("cv_bridge exception: %s", e.what());
+		return;
+	}
+	cv::imshow(BOTTOM_WINDOW_NAME, cvImagePtr->image);
+	cv::waitKey(3);
 }
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "offb_node");
     ros::NodeHandle nh;
+    image_transport::ImageTransport bottomImageTransport(nh);
+    cv::namedWindow(BOTTOM_WINDOW_NAME, cv::WINDOW_NORMAL);
 
     ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>
-            ("mavros/state", 10, state_cb);
+        ("mavros/state", 10, state_cb);
 
     ros::Subscriber camera_sub = nh.subscribe<sensor_msgs::Image>
-            ("iris_1/camera_down/image_raw", 10, camera_cb);
+        ("iris_1/camera_down/image_raw", 10, camera_cb);
 
     ros::Subscriber camera_info_sub = nh.subscribe<sensor_msgs::CameraInfo>
-            ("iris_1/camera_down/camera_info", 10, camera_info_cb);
+        ("iris_1/camera_down/camera_info", 10, camera_info_cb);
 
     ros::Subscriber tag_detect_sub = nh.subscribe<apriltag_ros::AprilTagDetectionArray>
-            ("/tag_detections", 10, &tag_detect_cb);
+        ("/tag_detections", 10, &tag_detect_cb);
 //ros::Subscriber apriltag_sub = nodeHandle.subscribe<apriltag_ros::AprilTagDetectionArray>
 //            ("apriltag_ros/AprilTagDetectionArray", 10, &apriltag_cb);
 
     ros::Subscriber coord_sub = nh.subscribe<geometry_msgs::PoseStamped>
-            ("keyboard_input/coord_msg", 10, coord_cb);
+        ("keyboard_input/coord_msg", 10, coord_cb);
+
+    image_transport::Subscriber bottomImageSubscriber = bottomImageTransport.subscribe
+        ("/tag_detections_image", 1, &bottomImageCallback);
+//        ("/iris_1/camera_down/image_raw", 1, &bottomImageCallback);
 
 
     image_pub = nh.advertise<sensor_msgs::Image>
-            ("/camera_rect/image_rect", 10);
+        ("/camera_rect/image_rect", 10);
     image_info_pub = nh.advertise<sensor_msgs::CameraInfo>
-            ("/camera_rect/camera_info", 10);
+        ("/camera_rect/camera_info", 10);
 
     ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>
-            ("mavros/setpoint_position/local", 10);
+        ("mavros/setpoint_position/local", 10);
     ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>
-            ("mavros/cmd/arming");
+        ("mavros/cmd/arming");
     ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>
-            ("mavros/set_mode");
+        ("mavros/set_mode");
 
     //the setpoint publishing rate MUST be faster than 2Hz
     ros::Rate rate(20.0);
